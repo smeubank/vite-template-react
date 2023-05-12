83faa33bf28bbd0e9b8e055ff1e9c9ecd4e5dfed
Steven Eubank <eubank.steven88@gmail.com>
2023-05-12 23:56:01 +0200

    add sentry
    with vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import * as Sentry from "@sentry/react";
 import App from './App';
+
+Sentry.init({
+  dsn: SENTRY_AUTH_TOKEN,
+  debug: true,
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    build: {
+      sourcemap: true, // Source map generation must be turned on
+    },
+    plugins: [
+      // https://vitejs.dev/config/
+      react(),
+      // Put the Sentry vite plugin after all other plugins
+      sentryVitePlugin({
+        org: "steven-eubank",
+        project: "admin-react-vite",
+
+        // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+        // and need `project:releases` and `org:read` scopes
+        authToken: env.SENTRY_AUTH_TOKEN,
+
+        sourcemaps: {
+          // Specify the directory containing build artifacts
+          assets: "./dist/**",
+        },
+
+        // Use the following option if you're on an SDK version lower than 7.47.0:
+        // include: "./dist",
+
+        // Optionally uncomment the line below to override automatic release name detection
+        // release: env.RELEASE,
+      }),
+    ],
+  };
 });
